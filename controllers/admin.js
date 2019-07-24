8107a06fe515d7f8108d87961b2e41104f9155b1
kuttyselva <kuttyselva57@gmail.com>
2019-07-25 00:28:51 +0530

    updated admin

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,26 +1,23 @@
-const Product = require('../models/product');
-const JSAlert = require("js-alert");
+const Product = require("../models/product");
+const alert = require("alert-node");
 exports.getAddPro = (req, res, next) => {
-    res.render('admin/add', { pagetitle: 'Add products', path: '/admin/add' });
+  res.render("admin/add", { pagetitle: "Add products", path: "/admin/add" });
 };
 exports.addNewPro = (req, res) => {
-    
-    const title = req.body.title;
-    const imageurl = req.body.imageurl;
-    const desc = req.body.desc;
-    const price = req.body.price;
-    const product=new Product(title,imageurl,desc,price);
-    product.save();
-    res.render("admin/add");
-    JSAlert.alert("Your files have been saved successfully.", "Files Saved", "Got it");
-    console.log('hii');
+  const title = req.body.title;
+  const imageurl = req.body.imageurl;
+  const desc = req.body.desc;
+  const price = req.body.price;
+  const product = new Product(title, imageurl, desc, price);
+  product.save();
+  res.render("admin/add");
 };
-exports.getProduct=(req,res)=>{
-    Product.fetchAll(products => {
-        res.render("admin/ad-pro-list", {
-          pro: products,
-          pagetitle: "Admin Products",
-          path: "/admin/pro"
-        });
-      });
-}
+exports.getProduct = (req, res) => {
+  Product.fetchAll(products => {
+    res.render("admin/ad-pro-list", {
+      pro: products,
+      pagetitle: "Admin Products",
+      path: "/admin/pro"
+    });
+  });
+};
